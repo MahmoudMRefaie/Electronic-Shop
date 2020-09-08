e21f6d2420d23df77530834cd51e4f03d3770ff6
MahmoudMRefaie <mahmoud.refaie22@gmail.com>
2020-09-08 22:22:05 +0200

    SignIn and SignUp

---
+++ app/src/main/java/com/mahmoudrefaie/mye_commerce/TermsPrivacy.kt
@@ -0,0 +1,11 @@
+package com.mahmoudrefaie.mye_commerce
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class TermsPrivacy : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_terms_privacy)
+    }
+}
